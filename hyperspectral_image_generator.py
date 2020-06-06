4247beb1685c36f9b27acd2dd62e9dbae03a9417
mabdelhack <m.abouelyazid@gmail.com>
2020-06-06 15:31:24 -0500

    Add JP2 generator

--- hyperspectral_image_generator.py
+++ hyperspectral_image_generator.py
@@ -48,6 +48,56 @@ def hyperspectral_image_generator(files, class_indices, batch_size=32, image_mea
         yield(X, Y)
 
 
+def hyperspectral_image_generator_jp2(files, shape_file, class_indices, batch_size=32, image_mean=None,
+                                      rotation_range=0, shear_range=0, scale_range=1,
+                                      transform_range=0, horizontal_flip=False,
+                                      vertical_flip=False, crop_size=None, filling_mode='edge',
+                                      speckle_noise=None):
+    from rasterio.mask import mask
+    import geopandas as gpd
+    import numpy as np
+    from random import sample
+    from image_functions import categorical_label_from_full_file_name, preprocessing_image_ms
+
+    geometry_df = gpd.read_file(shape_file)
+    centroids = geometry_df['geometry'].values
+    while True:
+        # select batch_size number of samples without replacement
+        batch_files = sample(files, batch_size)
+        # get one_hot_label
+        batch_Y = categorical_label_from_full_file_name(batch_files,
+                                                        class_indices)
+        # array for images
+        batch_X = []
+        # loop over images of the current batch
+        for idx, input_path in enumerate(batch_files):
+            image = np.array(imread(input_path), dtype=float)
+            if image_mean is not None:
+                mean_std_data = np.loadtxt(image_mean, delimiter=',')
+                image = preprocessing_image_ms(image, mean_std_data[0], mean_std_data[1])
+            # process image
+            image = augmentation_image_ms(image, rotation_range=rotation_range, shear_range=shear_range,
+                                          scale_range=scale_range,
+                                          transform_range=transform_range, horizontal_flip=horizontal_flip,
+                                          vertical_flip=vertical_flip, warp_mode=filling_mode)
+            if speckle_noise is not None:
+                from skimage.util import random_noise
+                image_max = np.max(np.abs(image), axis=(0, 1))
+                image /= image_max
+
+                image = random_noise(image, mode='speckle', var=speckle_noise)
+                image *= image_max
+
+            image = crop_image(image, crop_size)
+            # put all together
+            batch_X += [image]
+        # convert lists to np.array
+        X = np.array(batch_X)
+        Y = np.array(batch_Y)
+
+        yield(X, Y)
+
+
 def augmentation_image_ms(image, rotation_range=0, shear_range=0, scale_range=1, transform_range=0,
                           horizontal_flip=False, vertical_flip=False, warp_mode='edge'):
     from skimage.transform import AffineTransform, SimilarityTransform, warp
